b08c7d18e9711b68e0168e33df7c9259c6841488
Charles Jarvis <csjarvis43@gmail.com>
2022-10-10 14:49:28 -0600

    added iterative solution

--- breadthFirstValues.js
+++ breadthFirstValues.js
@@ -2,16 +2,31 @@
 
 // Iterative
 
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 
+const breadthFirstValues = (root) => {
+    if (root === null) return [];
 
+    const queue = [ root ];
+    let result = [];
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+        result.push(current.val);
+
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+    }
+
+    return result;
+}
 
 
 
