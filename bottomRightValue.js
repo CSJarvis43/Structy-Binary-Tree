a132725ebf69af8178b1821b6dcc0c3bb53e668b
Charles Jarvis <csjarvis43@gmail.com>
2022-10-14 16:04:14 -0600

    small change

--- bottomRightValue.js
+++ bottomRightValue.js
@@ -1,4 +1,5 @@
 // Write a function, bottomRightValue, that takes in the root of a binary tree. 
+
 // The function should return the right-most value in the bottom-most level of the tree.
 
 //You may assume that the input tree is non-empty.
